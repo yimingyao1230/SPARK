045fea7bef3e708cd485c3fbe60dd866a4f11d6b
Yiming Yao <yaoyiming20@gmail.com>
2024-11-07 22:33:04 -0500

    added midas to calculate the relative depth

--- run.py
+++ run.py
@@ -3,18 +3,38 @@ import os
 from ultralytics import YOLO
 import cv2
 
+from scripts.midas_core import MidasCore
 
 output_path = 'output'
 input_path = 'input'
 
 # Load the YOLOv11 model
-model_path = 'model/yolo11n.pt'
+model_path = 'models/yolo11n.pt'
 model = YOLO(model_path)
+
+midas_model_path = 'models/dpt_beit_large_512.pt'
+midas = MidasCore(midas_model_path)
 
 
 for file_name in os.listdir(input_path):
     file_path = os.path.join(input_path, file_name)
     results = model(file_path)
     annotated_img = results[0].plot()
+
+    for result in results:
+        boxes = result.boxes.xyxy.cpu().numpy()  # Bounding box coordinates (x_min, y_min, x_max, y_max)
+        confidences = result.boxes.conf.cpu().numpy()  # Confidence scores
+        class_ids = result.boxes.cls.cpu().numpy()  # Class IDs
+
+        # Print or process each detection
+        for box, confidence, class_id in zip(boxes, confidences, class_ids):
+            x_min, y_min, x_max, y_max = box  # Bounding box coordinates
+            print(f"Bounding Box: {x_min, y_min, x_max, y_max}, Confidence: {confidence}, Class ID: {class_id}")
+
+            depth = midas.get_depth(file_path, box)
+            depth_label = f"depth: {depth:.2f}"
+            x_center = int((x_min + x_max) / 2)
+            y_center = int((y_min + y_max) / 2)
+            cv2.putText(annotated_img, depth_label, (x_center, y_center), cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(255, 255, 255), thickness=2)
 
     cv2.imwrite(output_path+"/"+file_name, annotated_img)
---
+++ scripts/midas_core.py
@@ -0,0 +1,102 @@
+import torch
+import cv2
+import numpy as np
+
+from scripts.midas.model_loader import default_models, load_model
+
+first_execution = True
+class MidasCore():
+    def __init__(self, model_path, model_type="dpt_beit_large_512", optimize=False, height=None, square=False) -> None:
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model_path = model_path
+        self.model_type = model_type
+        self.model, self.transform, net_w, net_h = load_model(self.device, model_path, model_type, optimize, height, square)
+
+    def read_image(self, path):
+        """Read image and output RGB image (0-1).
+
+        Args:
+            path (str): path to file
+
+        Returns:
+            array: RGB image (0-1)
+        """
+        img = cv2.imread(path)
+
+        if img.ndim == 2:
+            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) / 255.0
+
+        return img
+
+    def normalize_depth(self, depth):
+        depth_min = depth.min()
+        depth_max = depth.max()
+        normalized_depth = 255 * (depth - depth_min) / (depth_max - depth_min)
+        normalized_depth *= 3
+
+        return normalized_depth
+        
+    def process(self, image, target_size, optimize=False, use_camera=False):
+        """
+        Run the inference and interpolate.
+
+        Args:
+            image: the image fed into the neural network
+            target_size: the size (width, height) the neural network output is interpolated to
+            optimize: optimize the model to half-floats on CUDA?
+            use_camera: is the camera used?
+
+        Returns:
+            the prediction
+        """
+        global first_execution
+
+        sample = torch.from_numpy(image).to(self.device).unsqueeze(0)
+
+        if optimize and self.device == torch.device("cuda"):
+            if first_execution:
+                print("  Optimization to half-floats activated. Use with caution, because models like Swin require\n"
+                    "  float precision to work properly and may yield non-finite depth values to some extent for\n"
+                    "  half-floats.")
+            sample = sample.to(memory_format=torch.channels_last)
+            sample = sample.half()
+
+        if first_execution or not use_camera:
+            height, width = sample.shape[2:]
+            print(f"    Input resized to {width}x{height} before entering the encoder")
+            first_execution = False
+
+        prediction = self.model.forward(sample)
+        prediction = (
+            torch.nn.functional.interpolate(
+                prediction.unsqueeze(1),
+                size=target_size[::-1],
+                mode="bicubic",
+                align_corners=False,
+            )
+            .squeeze()
+            .cpu()
+            .numpy()
+        )
+
+        return prediction
+
+    def get_depth(self, img, boxes):
+        original_image_rgb = self.read_image(img)  # in [0, 1]
+        # print("original_image_rgb shape ", original_image_rgb.shape)
+        image = self.transform({"image": original_image_rgb})["image"]
+        # print("image shape ", image.shape)
+
+        # compute
+        with torch.no_grad():
+            prediction = self.process(image, original_image_rgb.shape[1::-1])
+            normalized_depth = self.normalize_depth(prediction)
+
+        x_min, y_min, x_max, y_max = boxes
+        x_center = int((x_min + x_max) / 2)
+        y_center = int((y_min + y_max) / 2)
+
+        # print("depth : ", normalized_depth[y_center][x_center])
+        return normalized_depth[y_center][x_center]
